d964031f5e2e5ee217fb1bda72345627575dbdb9
Keenan <kpautler4@gmail.com>
2018-12-05 16:01:08 -0700

    can now make http requests to locally run restAPI which is connected to a local database, and can display the first name of the only user in the database under the click me button

--- src/containers/Profile/profile.js
+++ src/containers/Profile/profile.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import { Grid, Row, Col, Image } from 'react-bootstrap';
 import './profile.css';
+import axios from 'axios';
 
+var baseUrl = 'http://localhost:8080/api';
 export class Profile extends React.Component {
+	constructor () {
+		super();
+		this.state = {
+			user: ''
+		}
+	this.getUserInfo = this.getUserInfo.bind(this);
+	}
+	
+	getUserInfo() {
+		axios.get(baseUrl + '/users/jdoe')
+			.then(response => this.setState({user: response.data}));
+		console.log();
+	}
+
 	render() {
-		return(
+		return(	
 		<Grid>
 			<Row>
 				<Col xs={15} ms={4}>
 					<Image id="profileWallpaper" src="http://picsum.photos/1280/300/?random" rounded/>
 				</Col>
 				<Col xs={2} ms={2}>
-					<Image id="profilePicture" src="https://placekitten.com/160/160" circle />
+					<Image id="profilePicture" src="https://loremflickr.com/320/240/dog" circle />
 				</Col>
 			</Row>
-		</Grid>			
+			<div className='button__container'>
+      			<button className='button' onClick={this.getUserInfo}>
+        			Click Me
+      			</button> 
+				<p>{this.state.user.firstName}</p>
+    		</div>	
+		</Grid>	
 		)
 	}
 } 
